171c46ba6137d9192b1b826cf04806f68303c379
krishmungase <mungasekrishna8@gmail.com>
2024-09-06 00:08:20 +0530

    dashboard-links

---
+++ client/src/config/dashboard-links.js
@@ -0,0 +1,34 @@
+import { HiHome } from "react-icons/hi";
+import { IoMdNotifications } from "react-icons/io";
+import { IoPeopleOutline } from "react-icons/io5";
+import { BiMessageRounded } from "react-icons/bi";
+
+const dashboardLinks = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    icon: HiHome,
+  },
+  {
+    id: 2,
+    title: "Community",
+    path: "/community",
+    icon: IoPeopleOutline,
+  },
+  {
+    id: 3,
+    title: "Notifications",
+    path: "/notifications",
+    icon: IoMdNotifications
+  },
+  {
+    id: 4,
+    title: "Messaging",
+    path: "/messaging",
+    icon: BiMessageRounded
+  },
+];
+
+
+export default dashboardLinks
