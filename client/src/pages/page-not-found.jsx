9088df188f3b0c911e0eff742a7c85466116e8cd
krishmungase <mungasekrishna8@gmail.com>
2024-09-06 00:09:14 +0530

    404 page not found page

---
+++ client/src/pages/page-not-found.jsx
@@ -0,0 +1,27 @@
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
+
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button
+            type="primary"
+            onClick={() => navigate("/")}
+            className="px-6 rounded-full bg-[#2f667f] hover:bg-[#3d7e9c] text-white ring-0"
+          >
+            Back Home
+          </Button>
+        }
+      />
+    </div>
+  );
+};
+
+export default NotFoundPage;
