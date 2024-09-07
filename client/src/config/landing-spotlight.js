a9e8548bb9e821f5fa6310c52d3fe8da73fc8eee
krishmungase <mungasekrishna8@gmail.com>
2024-09-07 12:02:34 +0530

    static data for landing page

---
+++ client/src/config/landing-spotlight.js
@@ -0,0 +1,22 @@
+const LandingPageSpotlight = [
+  {
+    id: 1,
+    name: "Dr. Ramesh Kumar",
+    image: "https://img.freepik.com/free-photo/business-male-with-laptop_23-2148479539.jpg?t=st=1725690637~exp=1725694237~hmac=cd3fd2add7e8ecc758d8bd8ecfeb6ce02ebf3179dc001c1541d26d64a591200c&w=826",
+    description: "A renowned philatelist with over 1,000 rare stamps in his collection.",
+  },
+  {
+    id: 2,
+    name: "Ms. Anjali Patel",
+    image: "https://img.freepik.com/free-photo/smiley-businesswoman-posing-city-with-arms-crossed_23-2148767033.jpg?t=st=1725690659~exp=1725694259~hmac=8ab27901d2d3f8234f0811d523a5098bb18f6e4307cc789d31b0196afd1faaa9&w=900",
+    description: "A stamp collector specializing in Indian stamps from the pre-independence era.",
+  },
+  {
+    id: 3,
+    name: "Mr. Rajesh Singh",
+    image: "https://img.freepik.com/free-photo/handsome-corporate-man-real-estate-agent-assistant-smiling-hold-hands-together-how-may-i-help-you-smiling-friendly-polite-assist-customer-white-background_176420-45257.jpg?t=st=1725690605~exp=1725694205~hmac=c0c5ec9e24648e0f0a3e522f404b0a20fd69859a5536f86d529f46376cec30cf&w=1800",
+    description: "An expert in postal history with a focus on international postal exchanges.",
+  },
+]
+
+export default LandingPageSpotlight;
