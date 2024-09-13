95abc1918e8390032c78e4ad9a75b1e82aa0c26d
krishmungase <mungasekrishna8@gmail.com>
2024-09-13 09:53:07 +0530

    community layout

--- client/src/components/dashboard/community/index.jsx
+++ client/src/components/dashboard/community/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import PostSection from './components/post-section'
+import NewSection from './components/newsection'
 
 const Community = () => {
   return (
-    <div>Community</div>
+    <div className="flex justify-between items-start space-x-6 w-full p-6 rounded-lg">
+
+      <div className="w-full md:w-2/3 p-4 rounded-md">
+        <PostSection />
+      </div>
+
+      <div className="hidden md:block w-1/3  p-4 rounded-md shadow-md">
+        <NewSection />
+      </div>
+    </div>
+
   )
 }
 
