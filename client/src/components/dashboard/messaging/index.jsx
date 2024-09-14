88dc7f264b6d9dc026e9cb02c0b5861c20966d5c
KingShivamX <shivamhippalgave@gmail.com>
2024-09-14 12:04:11 +0530

    messenging info

--- client/src/components/dashboard/messaging/index.jsx
+++ client/src/components/dashboard/messaging/index.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Messaging = () => {
   return (
-    <div>Messaging</div>
+    <div className='p-4'>
+      <h1 className='text-lg py-4'>Work in progress</h1>
+      <p>User Schema, <br />
+      One to Many DB relation with Friends <br />
+      seperate Collection for Chats, with One to many relation Again <br />
+      Socket IO for Actual messaging</p> 
+      <input type="checkbox" /> check 
+    </div>
   )
 }
 
