33eab4d2f753d8f15f9d287022e09bfbb1f5e1cb
KingShivamX <shivamhippalgave@gmail.com>
2024-11-20 11:58:50 +0530

    yo2

--- client/src/layout/dashboard-layout.jsx
+++ client/src/layout/dashboard-layout.jsx
@@ -1,46 +1,45 @@
-import { Outlet, Link } from "react-router-dom";
-import { Avatar, Input } from "antd";
-import DashboardSidebar from "@/components/dashboard/dash-sidebar";
-import AiChatBot from "@/components/ui/components/ai-chat";
+import { Outlet, Link } from "react-router-dom"
+import { Avatar, Input } from "antd"
+import DashboardSidebar from "@/components/dashboard/dash-sidebar"
+import AiChatBot from "@/components/ui/components/ai-chat"
 
-const isAuthenticated = true;
+const isAuthenticated = true
 const user = {
-  fullName: "Krish Mungase",
-};
+    fullName: "King Shivam",
+}
 const DashboardLayout = () => {
-  return (
-    <div className="h-screen flex flex-row">
-      <DashboardSidebar />
-      <div className="flex flex-col w-[calc(100vw_-60px)] lg:w-[calc(100vw_-250px)]">
-        <div className="h-12 flex items-center justify-between px-6 space-x-6 border-b">
-          <div>
-            <Input.Search
-              className="max-w-sm sm:w-[300px]"
-              placeholder="search"
-            />
-          </div>
-          <div className="flex items-center justify-center space-x-4">
-            <Link to={'/dashboard/profile'}>
-              <div className="flex items-center justify-center space-x-1">
-                <Avatar className="bg-primary">
-                  {user?.fullName[0].toUpperCase()}
-                </Avatar>
-                <span className="text-primary text-sm font-medium hidden md:block">
-                  {user?.fullName}
-                </span>
-              </div>
-            </Link>
-          </div>
-        </div>
+    return (
+        <div className="h-screen flex flex-row">
+            <DashboardSidebar />
+            <div className="flex flex-col w-[calc(100vw_-60px)] lg:w-[calc(100vw_-250px)]">
+                <div className="h-12 flex items-center justify-between px-6 space-x-6 border-b">
+                    <div>
+                        <Input.Search
+                            className="max-w-sm sm:w-[300px]"
+                            placeholder="search"
+                        />
+                    </div>
+                    <div className="flex items-center justify-center space-x-4">
+                        <Link to={"/dashboard/profile"}>
+                            <div className="flex items-center justify-center space-x-1">
+                                <Avatar className="bg-primary">
+                                    {user?.fullName[0].toUpperCase()}
+                                </Avatar>
+                                <span className="text-primary text-sm font-medium hidden md:block">
+                                    {user?.fullName}
+                                </span>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
 
-        <div className="flex-1 overflow-y-auto bg-white relative">
-          <Outlet />
-          <AiChatBot />
+                <div className="flex-1 overflow-y-auto bg-white relative">
+                    <Outlet />
+                    <AiChatBot />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
-
-export default DashboardLayout;
+export default DashboardLayout
